f6d571c43bd4c58f9551c3b721b31b9727d4cc5f
cutright <dan.cutright@gmail.com>
2018-07-17 22:43:30 -0400

    initial development

--- containers/main/admin.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-dvh admin --allow-websocket-origin=localhost:5007 --port=5107
--- containers/main/settings.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-dvh settings --allow-websocket-origin=localhost:5008 --port=5108
